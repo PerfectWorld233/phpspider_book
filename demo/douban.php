ed7cf2a3bfcdfbe340578e8d0866a80d4e0d76d0
PerfectWorld233 <perfectworlddream@gmail.com>
2017-02-03 13:30:27 +0800

    test monolog

--- demo/douban.php
+++ demo/douban.php
@@ -6,11 +6,27 @@
  * Time: 21:07
  */
 
-require '../core/init.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
+
+/*require '../core/init.php';
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 echo "Connection to server sucessfully";
 //check whether server is running or not
-echo "Server is running: " . $redis->ping();
+echo "Server is running: " . $redis->ping();*/
 
+
+// Create the logger
+$logger = new Logger('my_logger');
+// Now add some handlers
+$logger->pushHandler(new StreamHandler(__DIR__.'/my_app.log', Logger::DEBUG));
+$logger->pushHandler(new FirePHPHandler());
+
+// You can now use your logger
+$logger->info('My logger is now ready');
+
+
+die;
 echo 124;die;
