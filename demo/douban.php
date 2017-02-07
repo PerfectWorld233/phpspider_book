4121be746e0f9ab4cbf2006cb3a5c878ea7734e2
nj_xuzhenyue <2694143802@qq.com>
2017-02-07 14:59:22 +0800

    redis test

--- demo/douban.php
+++ demo/douban.php
@@ -8,12 +8,12 @@
 
 
 require '../core/init.php';
-//$redis = new Redis();
-//$redis->connect('127.0.0.1', 6379);
-//echo "Connection to server sucessfully";
-////check whether server is running or not
-//echo "Server is running: " . $redis->ping();
-
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+echo "Connection to server sucessfully";
+//check whether server is running or not
+echo "Server is running: " . $redis->ping();
+die;
 
 require 'vendor/autoload.php';
 
